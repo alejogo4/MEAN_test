4229139bd39b3157164b408042c707965cc588db
Alejandro Giraldo Duque <alejo.go4@hotmail.com>
2019-09-24 20:54:24 -0500

    Agregar API y configuracion de conexion

---
+++ API/heros/heros.model.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var herosSchema = new Schema({
+    name: {
+        type: String,
+        unique: false,
+        required: true
+    },
+    description: {
+        type: String,
+        unique: false,
+        required: true
+    }
+
+});
+
+module.exports = herosSchema;
